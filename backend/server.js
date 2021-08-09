5daffbce75e38177d7a57acc3d048c5efd089f94
kaanfkennedy <dev.kaanfkennedy@gmail.com>
2021-08-09 14:49:54 +0300

    add listing files

--- backend/server.js
+++ backend/server.js
@@ -4,11 +4,12 @@ const multer = require('multer');
 const fs = require('fs');
 
 const { download } = require('./utils/download');
+const { listDir } = require('./utils/listDir');
 
 
 app.use(express.urlencoded({limit: '50mb',extended: false})); 
 app.use(express.json({limit: '50mb'}));
-
+app.use(express.static());
 
 const upload = multer();
 
@@ -29,7 +30,7 @@ app.post('/upload-file' , upload.single('file'), (req , res)=>{
     try {
         download(filePath, file.buffer);
         res.status(200);
-        res.send({'message': 'file was uploaded successfully', 'status': 200});
+        res.status({'message': 'file was uploaded successfully', 'status': 200});
     }catch(e) {
         res.send({'error': e})
         res.status(400);
@@ -55,6 +56,45 @@ app.post('/create-folder' , (req , res)=>{
     } 
 });
 
+
+app.get('/get-files/:folderName/:fileName' , (req , res)=>{
+    const folderName = req.params.folderName;
+    const fileName = req.params.fileName;
+    
+    const path = `/home/kaanfkennedy/Desktop/CloudPi/backend/${fileName}`;
+    const isFileExist = fs.existsSync(path);
+    
+    if(isFileExist) {
+        res.sendFile(path);
+        res.status(200);
+    }else {
+        res.send({'message': 'file is doesnt exist', status: 400});
+        res.status(400);
+    }
+});
+
+app.get('/get-all-files' , async (req , res)=>{
+    const files = listDir('/home/kaanfkennedy/Desktop/CloudPi/backend/');
+
+    res.send(files);
+});
+
+// shows the file
+app.get('/:fileName' , (req , res)=>{
+    const fileName = req.params.fileName;
+
+    const isFileExist = fs.existsSync(fileName);
+    
+    const path = `/home/kaanfkennedy/Desktop/CloudPi/backend/${fileName}`;
+    
+    if(isFileExist) {
+        res.sendFile(path);
+        res.status(200);
+    }else {
+        res.send({'message': 'file is doesnt exist' , 'status': 400});
+        res.status(400);
+    }
+});
 
 
 app.listen(3000);
